0be13bc519469585c1e09c2dd3a566b3e3440e79
Fritz Schrogl <fschrogl@users.noreply.github.com>
2020-10-08 00:15:27 +0200

    Added license information to project

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java 8 Examples
+
+Some little examples/demonstrations of new language features introduced
+with Java 8.
--- src/main/java/at/schrogl/playground/java8/bulkOperations/filterMapReduce/package-info.java
+++ src/main/java/at/schrogl/playground/java8/bulkOperations/filterMapReduce/package-info.java
@@ -11,3 +11,25 @@
  * Für ein simples Beispiel siehe {@link at.schrogl.playground.java8.bulkOperations.filterMapReduce.FilterMapReduce}
  */
 package at.schrogl.playground.java8.bulkOperations.filterMapReduce;
+
+/*-
+ * #%L
+ * Java 8 Examples
+ * %%
+ * Copyright (C) 2018 - 2020 Fritz Schrogl
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
